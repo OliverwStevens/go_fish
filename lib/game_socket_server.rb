e0af86235f1c7ea4cbde1fc7fbc5cd96206cfc48
Oliver Stevens <oliverwstevens@gmail.com>
2025-06-09 09:19:17 -0400

    Create basic server methods

--- lib/game_socket_server.rb
+++ lib/game_socket_server.rb
@@ -4,4 +4,36 @@ require_relative 'game'
 
 class GameSocketServer
   attr_accessor :server, :responses
+
+  def initialize
+    @responses = []
+  end
+
+  def port_number
+    3336
+  end
+
+  def clients
+    @clients ||= []
+  end
+
+  def players
+    @players ||= []
+  end
+
+  def start
+    @server = TCPServer.new(port_number)
+  end
+
+  def accept_new_client(player_name = 'Random Player')
+    client = server.accept_nonblock
+    puts 'Client accepted'
+    client.puts('Welcome to Go Fish!')
+    clients.push(client)
+  rescue IO::WaitReadable, Errno::EINTR
+  end
+
+  def stop
+    server&.close
+  end
 end
--- spec/game_socket_server_spec.rb
+++ spec/game_socket_server_spec.rb
@@ -1,3 +1,26 @@
-require_relative '../spec/game_socket_server_spec'
+require_relative '../lib/game_socket_server'
 require_relative '../lib/mock_game_socket_client'
 require_relative 'spec_helper'
+
+describe GameSocketServer do
+  let(:client1) { MockGameSocketClient.new(@server.port_number) }
+  let(:client2) { MockGameSocketClient.new(@server.port_number) }
+
+  before(:each) do
+    @clients = []
+    @server = GameSocketServer.new
+    @server.start
+  end
+
+  after(:each) do
+    @server.stop
+    @clients.each do |client|
+      client.close
+    end
+  end
+
+  it 'is not listening on a port before it is started' do
+    @server.stop
+    expect { MockGameSocketClient.new(@server.port_number) }.to raise_error(Errno::ECONNREFUSED)
+  end
+end
