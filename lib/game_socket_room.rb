a3f7936631a20cba4acaea73a26936f15d1c8d8b
Oliver Stevens <oliverwstevens@gmail.com>
2025-06-09 17:15:26 -0400

    Add game loop

--- lib/game_socket_room.rb
+++ lib/game_socket_room.rb
@@ -1,13 +1,14 @@
 require_relative 'game'
 class GameSocketRoom
   attr_reader :clients
-  attr_accessor :game, :rounds
+  attr_accessor :game, :rounds, :turn_over
 
   def initialize(clients)
     puts 'Created game'
     @game = Game.new(clients.count)
     @rounds = 0
     @clients = clients
+    @turn_over = false
   end
 
   def start_game
@@ -20,11 +21,11 @@ class GameSocketRoom
   end
 
   def run_game
+    start_game
     until game.game_end?
       # Loop here, this will be a turn
-      check_to_have_cards
-      display_cards
-      round
+
+      round until turn_over
     end
   end
 
@@ -36,11 +37,20 @@ class GameSocketRoom
     game.players[rounds % game.players.count]
   end
 
-  def round
+  def turn
     check_to_have_cards
+    display_cards
+    opponent, rank = player_choices
+    message = game.round(current_player, opponent, rank)
+    current_client.puts = message
+    current_client.puts(current_player.find_matches)
+
+    message
   end
 
-  def turn
+  def round
+    self.turn_over = true unless turn.match("rank #{rank}")
+    self.rounds += 1
   end
 
   def check_to_have_cards
@@ -49,7 +59,7 @@ class GameSocketRoom
     if game.deck.has_cards?
       current_client.puts game.draw_if_hand_empty(current_player)
     else
-      turn_over = true
+      self.turn_over = true
       current_client.puts 'The deck is out of cards, you are out of the game.'
     end
   end
@@ -62,25 +72,33 @@ class GameSocketRoom
   end
 
   def message_all_clients(message)
-    # binding.irb
     clients.each do |client|
       client.puts(message)
     end
   end
 
-  def get_opponent(current_client, current_player)
+  def player_choices
+    opponent, rank = []
+    until game.validate_input?(opponent, rank)
+      opponent = get_opponent
+      rank = get_rank
+    end
+    [opponent, rank]
+  end
+
+  def get_opponent
     current_client.puts 'What player do you want to ask for a card?'
-    opponent_input = listen_for_client(current_client) # gets.chomp
+    opponent_input = listen_for_client # gets.chomp
     game.return_opponent(current_player, opponent_input)
   end
 
-  def get_rank(current_client, current_player)
+  def get_rank
     current_client.puts 'What rank do you want?'
-    rank_input = listen_for_client(current_client) # gets.chomp
+    rank_input = listen_for_client # gets.chomp
     game.return_rank(current_player, rank_input)
   end
 
-  def listen_for_client(current_client)
+  def listen_for_client
     sleep(0.1)
     begin
       current_client.read_nonblock(1000)
--- spec/game_socket_room_spec.rb
+++ spec/game_socket_room_spec.rb
@@ -32,41 +32,41 @@ describe GameSocketRoom do
   it 'makes a room with 2 players' do
     expect(@room.game.players.count).to eql(2)
   end
-
-  it 'starts the game' do
-    @room.start_game
-    expect(client1.capture_output).to match(/the players/i)
-  end
   it '#message_all_clients' do
     @room.message_all_clients('Hello')
     expect(client1.capture_output).to match(/hello/i)
-    # expect(client2.capture_output).to match(/hello/i)
+    expect(client2.capture_output).to match(/hello/i)
   end
 
-  it 'gets the opponent' do
-    client1.provide_input('Player 2')
-    # binding.irb
-    expect(@room.get_opponent(@room.current_client, get_player(1))).to eql(get_player(2))
+  it '#start_game' do
+    @room.start_game
+    expect(client1.capture_output).to match(/the players/i)
   end
 
-  it 'gets the rank' do
-    get_player(0).hand = [PlayingCard.new('♥', '2')]
-    client1.provide_input('2')
-    expect(@room.get_rank(@room.current_client, get_player(0))).to eql('2')
-  end
-
-  it 'gets the current player' do
+  it '#current_player' do
     expect(@room.current_player).to eql(get_player(0))
   end
 
-  it 'checks to see that the current player has cards' do
+  it '#check_to_have_cards' do
     expect(@room.check_to_have_cards)
   end
 
-  it 'displays the cards' do
+  it '#display_cards' do
     @room.start_game
     @room.display_cards
     expect(client1.capture_output).to match(/of/i)
+  end
+
+  it '#get_opponent' do
+    client1.provide_input('Player 2')
+    # binding.irb
+    expect(@room.get_opponent).to eql(get_player(2))
+  end
+
+  it '#get_rank' do
+    get_player(0).hand = [PlayingCard.new('♥', '2')]
+    client1.provide_input('2')
+    expect(@room.get_rank).to eql('2')
   end
 
   xit 'plays a round' do
